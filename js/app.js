0d2c6b91178f3dd6a011f0c273ca1b30841a5f6b
Ryan Fuhrman <rsfuhrman10@gmail.com>
2018-02-15 11:19:25 -0500

    Add function to clear all and clear last entry

--- js/app.js
+++ js/app.js
@@ -7,6 +7,7 @@ const equations = {
 };
 
 // query selectors
+const screen = document.querySelector('.screen');
 const allClear = document.querySelector('.all_clear');
 const clearEntry = document.querySelector('.clear_entry');
 const divide = document.querySelector('.divide');
@@ -15,34 +16,40 @@ const subtract = document.querySelector('.add');
 const add = document.querySelector('.subtract');
 const equals = document.querySelector('.equals');
 const decimal = document.querySelector('.decimal');
-const zero = document.querySelector('.zero');
-const one = document.querySelector('.one');
-const two = document.querySelector('.two');
-const three = document.querySelector('.three');
-const four = document.querySelector('.four');
-const five = document.querySelector('.five');
-const six = document.querySelector('.six');
-const seven = document.querySelector('.seven');
-const eight = document.querySelector('.eight');
-const nine = document.querySelector('.nine');
 
 // event listeners
-allClear.addEventListener('click', () => console.log('Clear Log'));
-clearEntry.addEventListener('click', () => console.log('Clear Last Entry'));
 divide.addEventListener('click', () => console.log('/'));
 multiply.addEventListener('click', () => console.log('*'));
 subtract.addEventListener('click', () => console.log('-'));
 add.addEventListener('click', () => console.log('+'));
 equals.addEventListener('click', () => console.log('='));
 decimal.addEventListener('click', () => console.log('.'));
-zero.addEventListener('click', () => console.log(0));
-one.addEventListener('click', () => console.log(1));
-two.addEventListener('click', () => console.log(2));
-three.addEventListener('click', () => console.log(3));
-four.addEventListener('click', () => console.log(4));
-five.addEventListener('click', () => console.log(5));
-six.addEventListener('click', () => console.log(6));
-seven.addEventListener('click', () => console.log(7));
-eight.addEventListener('click', () => console.log(8));
-nine.addEventListener('click', () => console.log(9));
+
+// return number clicked
+function buttonClicked(e) {
+  // add number clicked to screen
+  if (e.target.classList.contains('num')) {
+    screen.textContent += e.target.textContent;
+  }
+}
+
+// clear screen
+function clearAll(e) {
+  if (e.target.classList.contains('all_clear')) {
+    screen.textContent = '';
+  }
+}
+
+// clear last entry
+function clearLastEntry(e) {
+  if (e.target.classList.contains('clear_entry')) {
+    const screenInput = Array.from(screen.textContent);
+    const indexOfLastItem = screenInput.length - 1;
+    screen.textContent = screenInput.splice(0, indexOfLastItem).join('');
+  }
+}
+
+document.addEventListener('click', buttonClicked);
+document.addEventListener('click', clearAll);
+document.addEventListener('click', clearLastEntry);
 
