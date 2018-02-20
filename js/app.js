8388f9ac6be6932897bd729216de81644d83fa43
Ryan Fuhrman <rsfuhrman10@gmail.com>
2018-02-20 12:03:15 -0500

    Bug fix for multiple operators in succession

--- js/app.js
+++ js/app.js
@@ -1,15 +1,7 @@
-
-const equations = {
-  add: (a, b) => a + b,
-  subtract: (a, b) => a - b,
-  multiply: (a, b) => a * b,
-  divide: (a, b) => a / b,
-};
-
 // query selectors
 const screen = document.querySelector('.screen');
-const allClear = document.querySelector('.all_clear');
-const clearEntry = document.querySelector('.clear_entry');
+const result = document.querySelector('.result');
+const equation = document.querySelector('.equation');
 const divide = document.querySelector('.divide');
 const multiply = document.querySelector('.multiply');
 const subtract = document.querySelector('.add');
@@ -17,39 +9,53 @@ const add = document.querySelector('.subtract');
 const equals = document.querySelector('.equals');
 const decimal = document.querySelector('.decimal');
 
-// event listeners
-divide.addEventListener('click', () => console.log('/'));
-multiply.addEventListener('click', () => console.log('*'));
-subtract.addEventListener('click', () => console.log('-'));
-add.addEventListener('click', () => console.log('+'));
-equals.addEventListener('click', () => console.log('='));
-decimal.addEventListener('click', () => console.log('.'));
-
 // return number clicked
-function buttonClicked(e) {
-  // add number clicked to screen
+function displayToScreen(e) {
+  // const buttonToNum = parseInt(e.target.textContent, 10);
+
+  // add button clicked to equations textContent
   if (e.target.classList.contains('num')) {
-    screen.textContent += e.target.textContent;
+    equation.textContent += e.target.textContent;
+  } else if (e.target.classList.contains('operator') && equation.textContent !== '') {
+    const checkForSpace = equation.textContent.split('');
+    const lastIndex = checkForSpace.length - 1;
+
+    if (checkForSpace[lastIndex] !== ' ') {
+      equation.textContent += ` ${e.target.textContent} `;
+    }
+  }
+
+  // make last button clicked results textContent
+  // if button clicked is number or operator
+  if (e.target.classList.contains('num' || 'operator')) {
+    result.textContent = e.target.textContent;
   }
 }
 
 // clear screen
 function clearAll(e) {
   if (e.target.classList.contains('all_clear')) {
-    screen.textContent = '';
+    equation.textContent = '';
   }
 }
 
 // clear last entry
 function clearLastEntry(e) {
   if (e.target.classList.contains('clear_entry')) {
-    const screenInput = Array.from(screen.textContent);
+    const screenInput = Array.from(equation.textContent);
     const indexOfLastItem = screenInput.length - 1;
-    screen.textContent = screenInput.splice(0, indexOfLastItem).join('');
+    equation.textContent = screenInput.splice(0, indexOfLastItem).join('');
   }
 }
 
-document.addEventListener('click', buttonClicked);
+function returnAnswer(e) {
+  if (e.target.classList.contains('equals')) {
+    result.textContent = 'lol';
+  }
+}
+
+document.addEventListener('click', displayToScreen);
 document.addEventListener('click', clearAll);
 document.addEventListener('click', clearLastEntry);
+document.addEventListener('click', returnAnswer);
 
