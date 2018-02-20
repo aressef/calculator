fd3c3d02ba7f1b29c9a85d8bc4194cbe943f4065
Ryan Fuhrman <rsfuhrman10@gmail.com>
2018-02-20 15:47:36 -0500

    Restructure functions into object methods for readability

--- js/app.js
+++ js/app.js
@@ -3,59 +3,57 @@ const result = document.querySelector('.result');
 const equation = document.querySelector('.equation');
 
 // return number clicked
-function displayToScreen(e) {
-  const errorMessage = 'Error: Limit Met';
-  // add button clicked to equations textContent
-  if (equation.textContent === errorMessage) {
-    equation.textContent = '';
-  }
 
-  if (equation.textContent.length < 25) {
-    if (e.target.classList.contains('num')) {
-      equation.textContent += e.target.textContent;
-    } else if (e.target.classList.contains('operator') && equation.textContent !== '') {
-      const checkForSpace = equation.textContent.split('');
-      const lastIndex = checkForSpace.length - 1;
+const view = {
+  displayEquation: (e) => {
+    const errorMessage = 'Error: Limit Met';
+    // add button clicked to equations textContent
+    if (equation.textContent === errorMessage) {
+      equation.textContent = '';
+    }
 
-      if (checkForSpace[lastIndex] !== ' ') {
-        equation.textContent += `${e.target.textContent}`;
+    if (equation.textContent.length < 25) {
+      if (e.target.classList.contains('num')) {
+        equation.textContent += e.target.textContent;
+      } else if (e.target.classList.contains('operator') && equation.textContent !== '') {
+        const checkForSpace = equation.textContent.split('');
+        const lastIndex = checkForSpace.length - 1;
+
+        if (checkForSpace[lastIndex] !== ' ') {
+          equation.textContent += `${e.target.textContent}`;
+        }
       }
+      // make last button clicked results textContent
+      // if button clicked is number or operator
+      if (e.target.classList.contains('num' || 'operator')) {
+        result.textContent = e.target.textContent;
+      }
+    } else {
+      equation.textContent = errorMessage;
     }
-    // make last button clicked results textContent
-    // if button clicked is number or operator
-    if (e.target.classList.contains('num' || 'operator')) {
-      result.textContent = e.target.textContent;
+  },
+  clearScreen: (e) => {
+    if (e.target.classList.contains('all_clear')) {
+      equation.textContent = '';
+      result.textContent = 0;
     }
-  } else {
-    equation.textContent = errorMessage;
-  }
-}
+  },
+  clearLastEntry: (e) => {
+    if (e.target.classList.contains('clear_entry')) {
+      const screenInput = Array.from(equation.textContent);
+      const indexOfLastItem = screenInput.length - 1;
+      equation.textContent = screenInput.splice(0, indexOfLastItem).join('');
+    }
+  },
+  displayAnswer: (e) => {
+    if (e.target.classList.contains('equals')) {
+      result.textContent = eval(equation.textContent);
+    }
+  },
+};
 
-// clear screen
-function clearAll(e) {
-  if (e.target.classList.contains('all_clear')) {
-    equation.textContent = '';
-    result.textContent = 0;
-  }
-}
-
-// clear last entry
-function clearLastEntry(e) {
-  if (e.target.classList.contains('clear_entry')) {
-    const screenInput = Array.from(equation.textContent);
-    const indexOfLastItem = screenInput.length - 1;
-    equation.textContent = screenInput.splice(0, indexOfLastItem).join('');
-  }
-}
-
-function returnAnswer(e) {
-  if (e.target.classList.contains('equals')) {
-    result.textContent = eval(equation.textContent);
-  }
-}
-
-document.addEventListener('click', displayToScreen);
-document.addEventListener('click', clearAll);
-document.addEventListener('click', clearLastEntry);
-document.addEventListener('click', returnAnswer);
+document.addEventListener('click', view.displayEquation);
+document.addEventListener('click', view.clearScreen);
+document.addEventListener('click', view.clearLastEntry);
+document.addEventListener('click', view.displayAnswer);
 
